11823fddbd2a18a72cb02d0c7d0d2fde5ab80b8d
Ray Sylverne <raysylverne@gmail.com>
2023-02-26 13:57:15 -0500

    organize dir

---
+++ terraform-data-sources-app/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    workspaces {
+      name = "learn-terraform-data-sources-app"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.45.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
