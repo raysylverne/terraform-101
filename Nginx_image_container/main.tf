38cd9bcdcc65fe1a1d1d002f677f4b167463d64f
Ray Sylverne <raysylverne@gmail.com>
2023-02-26 12:31:01 -0500

    add service and replica to nginx contianer

--- Nginx_image_container/main.tf
+++ Nginx_image_container/main.tf
@@ -1,24 +1,55 @@
+# Set the required provider and versions
 terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+      version = "3.0.1"
     }
   }
 }
 
+# Configure the docker provider
 provider "docker" {}
 
+# Create a docker image resource
+# -> docker pull nginx:latest
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
-  keep_locally = false
+  keep_locally = true
 }
 
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
 resource "docker_container" "nginx" {
-  name  = "nginx"
-  image = docker_image.nginx.latest
+  name    = "nginx"
+  image   = docker_image.nginx.latest
+
   ports {
+    external = 8080
     internal = 80
-    external = 8000
+  }
+}
+
+# Or create a service resource
+# -> same as 'docker service create -d -p 8081:80 --name nginx-service --replicas 2 nginx:latest'
+resource "docker_service" "nginx_service" {
+  name = "nginx-service"
+  task_spec {
+    container_spec {
+      image = docker_image.nginx.repo_digest
+    }
+  }
+
+  mode {
+    replicated {
+      replicas = 2
+    }
+  }
+
+  endpoint_spec {
+    ports {
+      published_port = 8081
+      target_port    = 80
+    }
   }
 }
