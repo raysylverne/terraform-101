c5f12780ff43c490b095930fee11eb531006cde3
Ray Sylverne <raysylverne@gmail.com>
2023-02-26 13:55:10 -0500

    add vpc, lb, ec2's, database,

---
+++ learn-terraform-data-sources-vpc/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    workspaces {
+      name = "learn-terraform-data-sources-vpc"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.45.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
---
+++ learn-terraform-outputs/outputs.tf
@@ -0,0 +1,29 @@
+# Output declarations
+output "vpc_id" {
+    description = "ID of project VPC"
+    value = module.vpc.vpc_id
+}
+
+output "lb_url" {
+  description = "URL of load balancer"
+  value       = "http://${module.elb_http.elb_dns_name}/"
+}
+
+output "web_server_count" {
+  description = "Number of web servers provisioned"
+  value       = length(module.ec2_instances.instance_ids)
+}
+
+/*You can designate Terraform outputs as sensitive. Terraform will redact the values 
+of sensitive outputs to avoid accidentally printing them out to the console.*/
+output "db_username" {
+    description = "Database administrator username"
+    value = aws_db_instance.database.username
+    sensitive = true
+}
+
+output "db_password" {
+    description = "Database administrator password"
+    value = aws_db_instance.database.password
+    sensitive = true
+}
