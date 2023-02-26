4280d18a358af94d1b6c2ba2de544bf6d7681d72
Ray Sylverne <raysylverne@gmail.com>
2023-02-26 13:58:57 -0500

    remove learn-terraform-data-sources-app

--- terraform-data-sources-app/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lb_url" {
-  description = "URL of load balancer"
-  value       = "http://${module.elb_http.elb_dns_name}/"
-}
-
-output "web_instance_count" {
-  description = "Number of EC2 instances"
-  value       = length(aws_instance.app)
-}
