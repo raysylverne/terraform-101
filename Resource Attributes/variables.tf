dbac51bd054d2fe406150a889f3d7f2a57dac89e
Ray Sylverne <raysylverne@gmail.com>
2023-02-26 20:57:48 -0500

    resource attribute 101

---
+++ Resource Attributes/variables.tf
@@ -0,0 +1,20 @@
+variable "filename" {
+  default = "/Users/raysylver/Desktop/pets.txt"
+}
+
+variable "content" {
+    default = "We love pets!"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}
+
