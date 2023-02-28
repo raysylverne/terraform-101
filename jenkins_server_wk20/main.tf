1ecd9dc358388f15a25b04372051b908539f5ebb
Ray Sylverne <raysylverne@gmail.com>
2023-02-28 11:42:39 -0500

    organized code

--- jenkins_server_wk20/main.tf
+++ jenkins_server_wk20/main.tf
@@ -1,22 +1,35 @@
 # VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
+
   tags = {
     Name = var.vpc_tag
+  }
+}
+
+# Subnet
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_tag
   }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc.id
+
   tags = {
     Name = var.internet_gateway_tag
   }
 }
 
 # Route Table
-resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_default_route_table" "default_route" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -24,22 +37,20 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-# Subnet
-resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = "us-east-1a"
-  tags = {
-    Name = var.subnet_tag
-  }
+# Public IP and add to Security Group
+
+data "external" "myipaddr" {
+  program = ["bash", "-c", "curl -s 'https://ipinfo.io/json'"]
 }
 
 # Security Group
-resource "aws_security_group" "security_group" {
+resource "aws_security_group" "jenkins_security_group" {
   name_prefix = var.security_group_name
+  description = "Jenkins EC2 instance"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
+    description = "Allow SSH from my Public IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -47,8 +58,17 @@ resource "aws_security_group" "security_group" {
   }
 
   ingress {
+    description = "Allows Access to the Jenkins Server"
     from_port   = 8080
     to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allows Access to the Jenkins Server"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -58,6 +78,9 @@ resource "aws_security_group" "security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Jenkins_Security_Group"
   }
 }
 
@@ -110,13 +133,13 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
 
 # EC2 Instance
 resource "aws_instance" "ec2_instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = var.ssh_key_name
-  vpc_security_group_ids = [aws_security_group.security_group.id]
-  subnet_id              = aws_subnet.subnet.id
-  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
-  user_data              = var.ec2_user_data
+  ami                  = var.ami
+  instance_type        = var.instance_type
+  key_name             = var.ssh_key_name
+  security_groups      = [aws_security_group.jenkins_security_group.id]
+  subnet_id            = aws_subnet.subnet.id
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  user_data            = var.ec2_user_data
   tags = {
     Name = var.ec2_tag
   }
@@ -124,10 +147,14 @@ resource "aws_instance" "ec2_instance" {
 
 # S3 Bucket
 resource "aws_s3_bucket" "s3" {
-  bucket        = var.bucket_name
-  force_destroy = true
+  bucket = var.bucket_name
 
   tags = {
     Name = var.bucket_name
   }
+}
+
+resource "aws_s3_bucket_acl" "jenkinsbucketacl" {
+  bucket = var.bucket_name
+  acl    = "private"
 }
