8502957a9168e46b422c1f9db737f4ac98c9fedf
Ray Sylverne <raysylverne@gmail.com>
2023-02-27 23:56:43 -0500

    updated version

--- AWS_Jenkins_Server/modules/jenkins_server/providers.tf
+++ AWS_Jenkins_Server/modules/jenkins_server/providers.tf
@@ -1,13 +1,12 @@
-#Create a providers file 
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.70.0"
+      version = "~> 4.55.0"
     }
   }
+
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
