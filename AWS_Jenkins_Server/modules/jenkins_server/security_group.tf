5e1393e37c7195c9cb62e001d67ba38cf25cf2c8
Ray Sylverne <raysylverne@gmail.com>
2023-02-27 21:32:50 -0500

    add variables cidr, ingress

--- AWS_Jenkins_Server/modules/jenkins_server/security_group.tf
+++ AWS_Jenkins_Server/modules/jenkins_server/security_group.tf
@@ -5,9 +5,8 @@ module "jenkins_security_group" {
   name_prefix = "jenkins-server"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["${var.my_ip}/32"]
-  ingress_rules       = [{ from_port = 22, to_port = 22, protocol = "tcp" }, 
-  { from_port = 8080, to_port = 8080, protocol = "tcp" }]
+  ingress_cidr_blocks = [var.ingress_cidr_blocks]
+  ingress_rules       = [{ from_port = var.from_port, to_port = var.to_port, protocol = "tcp" }]
 
   tags = {
     Terraform   = "true"
